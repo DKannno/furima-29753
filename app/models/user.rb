74e115b83c6c9145eb8cc92e5c50a7a4338d86ba
DKannno <kannno.0810@gmail.com>
2020-10-05 14:44:55 +0900

    with_optionでバリデーション整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   has_many :purchases
-  validates :nickname, presence: true
-  validates :email, format: { with: /\A.+[@].+\z/ }, uniqueness: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6}/i }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
-  validates :birth_day, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :email,  format: { with: /\A.+[@].+\z/ }, uniqueness: true
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6}/i }
+    validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :last_name_kana,  format: { with: /\A[ァ-ン]+\z/ }
+    validates :first_name_kana,  format: { with: /\A[ァ-ン]+\z/ }
+    validates :birth_day
+  end
 end
