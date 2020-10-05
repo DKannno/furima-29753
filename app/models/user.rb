8244c2d60b8dfa16eb0cef113f74e6279f0c34d6
DKannno <kannno.0810@gmail.com>
2020-10-05 15:43:19 +0900

    共通の正規表現を変数化に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   has_many :purchases
+
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  NAME_KANA_REGEX = /\A[ァ-ン]+\z/
   with_options presence: true do
     validates :nickname
     validates :email,  format: { with: /\A.+[@].+\z/ }, uniqueness: true
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6}/i }
-    validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :last_name_kana,  format: { with: /\A[ァ-ン]+\z/ }
-    validates :first_name_kana,  format: { with: /\A[ァ-ン]+\z/ }
+    validates :last_name,  format: { with: NAME_REGEX}
+    validates :first_name,  format: { with: NAME_REGEX}
+    validates :last_name_kana,  format: { with: NAME_KANA_REGEX }
+    validates :first_name_kana,  format: { with:NAME_KANA_REGEX }
     validates :birth_day
   end
 end
