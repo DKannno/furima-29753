a383904b3a02589c206b1741f10fee35d403533e
DKannno <kannno.0810@gmail.com>
2020-10-14 16:58:13 +0900

    環境変数の設定（公開鍵、秘密鍵）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,11 +4,33 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
-  # def create
-  # end
+  def create
+    @order =Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
 
 
   private
- 
+   
+  def order_params 
+    params.require(:order).maerge(item_id: item.id, user_id: user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: order_params[:price],  
+        card: order_params[:token], 
+        currency: 'jpy' 
+      )
+  end
+  
+
 
 end
