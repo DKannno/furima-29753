b25ada420b7633ea68d1608ef897ee3e446e3bfe
DKannno <kannno.0810@gmail.com>
2020-10-16 05:58:36 +0900

    購入情報の保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,17 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_address =OrderAddress.new
   end
 
   def create
-    @order =Order.new(order_params)
-    if @order.valid?
+    @item = Item.find(params[:item_id])
+    @order_address =OrderAddress.new(order_params)
+   
+    if @order_address.valid?
       pay_item
-      @order.save
+      @order_address.save
+    binding.pry
       return redirect_to root_path
     else
       render 'index'
@@ -17,15 +21,21 @@ class OrdersController < ApplicationController
 
 
   private
+
+  # def set_item
+  #   @item = Item.find(params[:item_id])
+  # end
+
    
   def order_params 
-    params.require(:order).maerge(item_id: item.id, user_id: user.id, token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_number, :orders_id ,:item_id).merge(token: params[:token],user_id: current_user.id)
   end
+
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
-        amount: order_params[:price],  
+        amount: @item[:price],  
         card: order_params[:token], 
         currency: 'jpy' 
       )
@@ -34,3 +44,4 @@ class OrdersController < ApplicationController
 
 
 end
+
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,12 +8,13 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]"),
-      cvc: formData.get("order[cvc]"),
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
+      number: formData.get("order_address[number]"),
+      cvc: formData.get("order_address[cvc]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
     };
-
+    
+    //  Payjp.createToken(カードの情報, 取得した後の処理） => {});
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
@@ -21,11 +22,11 @@ const pay = () => {
         const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-
-      document.getElementById("order_number").removeAttribute("name");
-      document.getElementById("order_cvc").removeAttribute("name");
-      document.getElementById("order_exp_month").removeAttribute("name");
-      document.getElementById("order_exp_year").removeAttribute("name");
+// クレジット情報削除name属性スペて
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
