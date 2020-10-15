f44f35464ddd248315aba37e5f121106c3095b0a
DKannno <kannno.0810@gmail.com>
2020-10-16 06:00:45 +0900

    prefecture_idにカラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_10_13_081031) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
-    t.integer "prefectures_id", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "house_number", null: false
     t.string "building_name"
